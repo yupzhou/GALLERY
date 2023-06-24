0e5539c352f250b2d578b37397e60630979e8a50
unknown <yupzhou@foxmail.com>
2023-06-24 20:48:59 +0800

    230624 v1.0

---
+++ assets/js/change.js
@@ -0,0 +1,101 @@
+function changeimg1(){
+	var x = document.getElementById("1z_s").value;
+	document.getElementById("1z").src = x;
+	var x = document.getElementById("1t_s").value;
+	document.getElementById("1t").src = x;
+	var checkbox = document.getElementById('1tu_s');
+	var x = document.getElementById('1tu');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/torn_u.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  }
+	var checkbox = document.getElementById('1td_s');
+	var x = document.getElementById('1td');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/torn_d.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  } 
+	var x = document.getElementById("1s_s").value;
+	document.getElementById("1s").src = x;
+	var checkbox = document.getElementById('1r1_s');
+	var x = document.getElementById('1r1');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/r1.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  } 
+	var checkbox = document.getElementById('1r2_s');
+	var x = document.getElementById('1r2');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/r2.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  } 
+	var checkbox = document.getElementById('1drip_s');
+	var x = document.getElementById('1drip');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/drip.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  } 
+}
+
+function changeimg2(){
+	var x = document.getElementById("2z_s").value;
+	document.getElementById("2z").src = x;
+	var x = document.getElementById("2t_s").value;
+	document.getElementById("2t").src = x;
+	var checkbox = document.getElementById('2tu_s');
+	var x = document.getElementById('2tu');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/torn_u.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  }
+	var checkbox = document.getElementById('2td_s');
+	var x = document.getElementById('2td');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/torn_d.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  } 
+	var x = document.getElementById("2s_s").value;
+	document.getElementById("2s").src = x;
+	var checkbox = document.getElementById('2r1_s');
+	var x = document.getElementById('2r1');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/r1.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  } 
+	var checkbox = document.getElementById('2r2_s');
+	var x = document.getElementById('2r2');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/r2.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  } 
+	var checkbox = document.getElementById('2cd_s');
+	var x = document.getElementById('2cd');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/cd.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  }
+	var checkbox = document.getElementById('2drip_s');
+	var x = document.getElementById('2drip');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/drip.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  }
+	var checkbox = document.getElementById('2sem_s');
+	var x = document.getElementById('2sem');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230623/sem.png';
+	  } else {
+	    x.src = 'assets/img/230623/none.png';
+	  }
+}

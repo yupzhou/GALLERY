57c178349254c5d06d10b80428e2643454d3f354
unknown <yupzhou@foxmail.com>
2023-06-25 23:58:09 +0800

    230625 v1.0

--- assets/js/change.js
+++ assets/js/change.js
@@ -1,4 +1,62 @@
-function changeimg1(){
+//200624 Arisa
+function changeimg200624(){
+	var x = document.getElementById("1z_s").value;
+	document.getElementById("1z").src = x;
+	var checkbox = document.getElementById('1c_s');
+	var x = document.getElementById('1c');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/200624/c.png';
+	  } else {
+	    x.src = 'assets/img/200624/none.png';
+	  }
+	var checkbox = document.getElementById('1doll_s');
+	var x = document.getElementById('1doll');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/200624/doll.png';
+	  } else {
+	    x.src = 'assets/img/200624/none.png';
+	  } 
+	var checkbox = document.getElementById('1sem_s');
+	var x = document.getElementById('1sem');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/200624/sem.png';
+	  } else {
+	    x.src = 'assets/img/200624/none.png';
+	  } 
+}
+
+//230507 Barbara
+function changeimg230507_1(){
+	var x = document.getElementById("1z_s").value;
+	document.getElementById("1z").src = x;
+	var x = document.getElementById("1t_s").value;
+	document.getElementById("1t").src = x;
+	var x = document.getElementById("1k_s").value;
+	document.getElementById("1k").src = x;
+	var x = document.getElementById("1g_s").value;
+	document.getElementById("1g").src = x;
+	var x = document.getElementById("1s_s").value;
+	document.getElementById("1s").src = x;
+}
+
+function changeimg230507_2(){
+	var x = document.getElementById("2z_s").value;
+	document.getElementById("2z").src = x;
+	var x = document.getElementById("2k_s").value;
+	document.getElementById("2k").src = x;
+	var x = document.getElementById("2g_s").value;
+	document.getElementById("2g").src = x;
+	var checkbox = document.getElementById('2sem_s');
+	var x = document.getElementById('2sem');
+	  if (checkbox.checked) {
+		x.src = 'assets/img/230507/sem.png';
+	  } else {
+	    x.src = 'assets/img/230507/none.png';
+	  }
+}
+
+//230623 Siver Hair Girl
+function changeimg230623_1(){
 	var x = document.getElementById("1z_s").value;
 	document.getElementById("1z").src = x;
 	var x = document.getElementById("1t_s").value;
@@ -42,7 +100,7 @@ function changeimg1(){
 	  } 
 }
 
-function changeimg2(){
+function changeimg230623_2(){
 	var x = document.getElementById("2z_s").value;
 	document.getElementById("2z").src = x;
 	var x = document.getElementById("2t_s").value;
